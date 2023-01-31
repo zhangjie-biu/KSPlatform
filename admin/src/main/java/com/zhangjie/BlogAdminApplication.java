cb8fd7fbb1868c6d6f8328d314e6780c3a935056
zhangjie <zhangjie.meet@gmail.com>
2023-01-31 15:28:33 +0800

    admin启动类

---
+++ admin/src/main/java/com/zhangjie/BlogAdminApplication.java
@@ -0,0 +1,14 @@
+package com.zhangjie;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.zhangjie.mapper")
+public class BlogAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAdminApplication.class,args);
+    }
+}
